fef98e66b448a1df6619087e5efa54f5e766b217
iHok <ihokya@gmail.com>
2020-02-10 11:44:18 +0900

    とりあえずawait/asyncが成功

--- docs/assets/googleSpreadSheetApiV3Getter.js
+++ docs/assets/googleSpreadSheetApiV3Getter.js
@@ -1,4 +1,4 @@
-const gSpreGet = (inputValue) => {
+const gSpreGetSheets = (inputValue) => {
     let inputValueSplit = inputValue.split("/");
     if (inputValueSplit[2] === "docs.google.com" && inputValueSplit[3] === "spreadsheets") {
         var url = "https://spreadsheets.google.com/feeds/worksheets/" + inputValueSplit[5] + "/public/basic?alt=json";
@@ -6,7 +6,17 @@ const gSpreGet = (inputValue) => {
       return fetch(url)
         .then((response) => {return response.json()})
      } else {
-      console.log("スプレッドシートのURLではありません")
+      console.log("this is not URL of GoogleSpreadSheets.")
       return false;
     }
+}
+
+const gSpreGetCells = (inputValue) => {
+  let inputValueSplit = inputValue.split("/");
+  console.log("inputValue");
+  console.log(inputValue);
+  var url = "https://spreadsheets.google.com/feeds/cells/" + inputValue + "/public/values?alt=json";
+  console.log(url);
+  return fetch(url)
+    .then((response) => {return response.json()})
 }
--- docs/assets/spreadsheetsScript.js
+++ docs/assets/spreadsheetsScript.js
@@ -1,49 +1,34 @@
-const changeSelectLists = (inputValue) => {
-    document.getElementById("sel1").innerHTML = '<option value="">選択して下さい</option>';
-    let retrunJson = gSpreGet(inputValue);
-    console.log("retrunJson");
-    console.log(gSpreGet(inputValue));
-    for (var i = 0; i < retrunJson.feed.entry.length; i++) {
-        let op = document.createElement("option");
-        op.value = inputValueSplit[5]+"/"+retrunJson.feed.entry[i].link[1].href.split("/")[6];  //value値
-        op.text = retrunJson.feed.entry[i].content.$t;   //テキスト値
-        document.getElementById("sel1").appendChild(op);
-    }
+const changeSelectLists = async (inputValue) => {
+  document.getElementById("sel1").innerHTML = '<option value="">選択して下さい</option>';
+  let inputValueSplit = inputValue.split("/");
+  let retrunJson = await gSpreGetSheets(inputValue);
+  console.log("retrunJson");
+  console.log(retrunJson);
+  for (var i = 0; i < retrunJson.feed.entry.length; i++) {
+    let op = document.createElement("option");
+    op.value = inputValueSplit[5]+"/"+retrunJson.feed.entry[i].link[1].href.split("/")[6];  //value値
+    op.text = retrunJson.feed.entry[i].content.$t;   //テキスト値
+    document.getElementById("sel1").appendChild(op);
   }
+}
   
-  const createTable = (sheetValue) => {
+const createTable = async (sheetValue) => {
     // 値(数値)から値(value値)を取得
-    const str = "https://spreadsheets.google.com/feeds/cells/" + sheetValue + "/public/values?alt=json";
-    console.log(str);
-    const result = document.getElementById("result");
-    document.getElementById("table").innerHTML = '';
-    fetch(str)
-      .then((response) => response.json())
-      .then((json) => {
-        //      console.log(Object.keys(json.feed.entry[1].filter(gsx => gsx.test(/gsx/))));
-        //      console.log(Object.keys(json.feed.entry[0]).filter(gsx => /gsx\$/.test(gsx)));
-        //.filter(gsx => /gsx\$/.test(gsx))
-        console.log("json");
-        jsonFeedEntryToTable(json.feed.entry);
-  
-        let tableData = (new Array(Number(json.feed.gs$rowCount.$t))).fill("").map(() => (new Array(Number(json.feed.gs$colCount.$t))).fill(""));
-        console.log(tableData);
-   
-        for (let i = 0; i < json.feed.entry.length; i++) {
-          //        table.push();
-          //        console.log(json.feed.entry[i]);
-  
-  
-  
-          let tr = document.createElement("tr");
-          let td = document.createElement("td");
-          td.appendChild(document.createTextNode(json.feed.entry[i].content.$t));  //value値
-          //            li.text = json.feed.entry[i].content.$t;   //テキスト値
-          document.getElementById("table").appendChild(tr).appendChild(td);
-        }
-  
-      });
+  let retrunCellsJson = await gSpreGetCells(sheetValue);
+  document.getElementById("table").innerHTML = '';
+  let tableData = (new Array(Number(retrunCellsJson.feed.gs$rowCount.$t))).fill("").map(() => (new Array(Number(retrunCellsJson.feed.gs$colCount.$t))).fill(""));
+  console.log(tableData);
+
+  for (let i = 0; i < retrunCellsJson.feed.entry.length; i++) {
+
+      let tr = document.createElement("tr");
+      let td = document.createElement("td");
+      td.appendChild(document.createTextNode(retrunCellsJson.feed.entry[i].content.$t));  //value値
+      //            li.text = retrunCellsJson.feed.entry[i].content.$t;   //テキスト値
+      document.getElementById("table").appendChild(tr).appendChild(td);
     }
+
+}
   
   window.onload = function () {
     changeSelectLists(document.querySelector("#spreadsheetsURL").value);
